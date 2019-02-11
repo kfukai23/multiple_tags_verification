7f82eb19aa9181300b2e68d6796faf5fa74a0a80
koalamask <yaiag04@gmail.com>
2019-02-11 21:41:41 +0900

    Add relations and class method

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,21 @@
 class Article < ApplicationRecord
+  has_many :categories, through: :article_categories
+  has_many :article_categories, dependent: :destroy
+
+  def save_categories(tags)
+    current_tags = self.categories.pluck(:name) unless self.categories.nil?
+    old_tags = current_tags - tags
+    new_tags = tags - current_tags
+
+    # Destroy old taggings:
+    old_tags.each do |old_name|
+      self.categories.delete Category.find_by(name:old_name)
+    end
+
+    # Create new taggings:
+    new_tags.each do |new_name|
+      article_category = Category.find_or_create_by(name:new_name)
+      self.categories << article_category
+    end
+  end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,5 @@
 class Category < ApplicationRecord
+  validates :name,presence:true,length:{maximum:50}
+  has_many :articles, through: :articles_categories
+  has_many :articles_categories, dependent: :destroy
 end
