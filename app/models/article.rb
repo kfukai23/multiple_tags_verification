da526888cea6be78b11ba06ab1ff635e47fceecf
koalamask <yaiag04@gmail.com>
2019-02-13 08:32:36 +0900

    同一ユーザーは同じ名称のタグを一つしか持てない・フリーのタグは削除されるよう変更

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,21 +1,24 @@
 class Article < ApplicationRecord
+  # around_destroy :check_free_tags
   has_many :article_categories, dependent: :destroy
   has_many :categories, through: :article_categories
   accepts_nested_attributes_for :article_categories
 
-  def save_categories(tags)
+  def save_categories(tags, current_user)
+    tags.uniq!
     current_tags = self.categories.pluck(:name) unless self.categories.nil?
     old_tags = current_tags - tags
     new_tags = tags - current_tags
 
     # Destroy old taggings:
     old_tags.each do |old_name|
-      self.categories.delete Category.find_by(name:old_name)
+      self.categories.delete current_user.categories.find_by(name:old_name)
+      current_user.categories.find_by(name:old_name).delete if current_user.categories.find_by(name:old_name).article_categories.blank?
     end
 
     # Create new taggings:
     new_tags.each do |new_name|
-      article_category = Category.find_or_create_by(name:new_name)
+      article_category = current_user.categories.find_or_create_by(name:new_name)
       self.categories << article_category
     end
   end
--- app/models/article_category.rb
+++ app/models/article_category.rb
@@ -3,4 +3,11 @@ class ArticleCategory < ApplicationRecord
   belongs_to :category
   validates :article_id,presence:true
   validates :category_id,presence:true
+  before_destroy :check_free_tag
+
+  def check_free_tag
+    self.category.destroy if self.category.article_categories.count == 1
+  end
+
+
 end
